95c8c8dc447f1ac6cdfd2e4412afff8e5889e2b5
ExtraAri <1117813@apps.nsd.org>
2025-09-12 16:49:49 +0000

    cool

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,25 @@
-// Student name
-// class period
-// project name
-// date
+// Ari Bernstein
+// P2
+// Zero-Hero
+// 9/12/25
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: whatever Mr Reiber says
+ * INPUT: We don't know about input yet
+ * OUTPUT:  Some cool lines in the command line terminal thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+  class Main{
+
+    public static void main(String[] args ){
+      System.out.println("-----------------------");
+      System.out.println("!!!! PROGRAM START !!!!");
+      System.out.println("Hello");
+      System.out.println("!!!! PROGRAM END !!!!");
+      System.out.println("-----------------------");
+    }
+
+  }
